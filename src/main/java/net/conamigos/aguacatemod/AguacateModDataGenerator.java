c73fc6f081a2cf5a3323409a69ec2bc13850af60
DarienMSA <sadarien@gmail.com>
2025-07-24 14:50:03 -0600

    Proyecto Inicial
    20250724 - Incluye las texturas necesarias para el Aguacate

---
+++ src/main/java/net/conamigos/aguacatemod/AguacateModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.conamigos.aguacatemod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AguacateModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
