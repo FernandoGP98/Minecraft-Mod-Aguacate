35633858f75796a67206d1ed64af026181e07dab
Fernando <fer_char98@hotmail.com>
2025-09-09 22:18:08 -0600

    Registro generito de ingredientes

---
+++ src/main/java/net/conamigos/aguacatemod/item/Ingredients/IngredientItems.java
@@ -0,0 +1,28 @@
+package net.conamigos.aguacatemod.item.Ingredients;
+
+import net.conamigos.aguacatemod.AguacateMod;
+import net.conamigos.aguacatemod.item.Ingredients.AvocadoItem.AvocadoItem;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class IngredientItems {
+
+    protected static Item register(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(AguacateMod.MOD_ID, name), item);
+    }
+
+    public static void registerAll() {
+        // 1) Registrar TODOS los items aquí (mientras los registries no están congelados)
+        AvocadoItem.register();
+        // Si luego hay más: SalsaRojaItem.register(); CarneAsadaItem.register(); ...
+
+        // 2) Ahora sí podemos agregarlos al creative tab (solo lectura)
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(AvocadoItem.AVOCADO);
+        });
+    }
+}
